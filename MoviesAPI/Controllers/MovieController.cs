9cc870214a64925b11a0c153d6fe42342d1666a1
Daria Pionk <dariapionk13@gmail.com>
2024-11-11 20:13:33 +0100

    Init .NET API project

---
+++ MoviesAPI/Controllers/MovieController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviesAPI.Entities;
+
+namespace MoviesAPI.Controllers
+{   
+    [Route("/api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAllMovies()
+        {
+            var movies = new List<Movie>
+            {
+                new Movie
+                {
+                    Id = 1,
+                    year = 2020,
+                    rate = 1.0
+                }
+            };
+
+            return Ok(movies);
+        }
+    }
+}
---
+++ MoviesAPI/Entities/Movie.cs
@@ -0,0 +1,12 @@
+﻿namespace MoviesAPI.Entities
+{
+    public class Movie
+    {
+        public required int Id { get; set; }
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public required int year { get; set; }
+        public required double rate { get; set; }
+
+    }
+}
