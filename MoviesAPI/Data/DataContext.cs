7f2d12ecd79d7896b196eeabfaed9a04b47d6ca4
Daria Pionk <dariapionk13@gmail.com>
2024-11-11 20:39:33 +0100

    Added DataContext

---
+++ MoviesAPI/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoviesAPI.Entities;
+
+namespace MoviesAPI.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
